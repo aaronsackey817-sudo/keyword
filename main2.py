84ab1075d20a1afe73686b047839b25ffb2c53bd
aaronsackey817-sudo <aaronsackey817@gmail.com>
2026-04-17 18:54:52 +0000

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,6 @@
+x=input("enter a number: ")
+if x=="5":
+    pass
+else:
+    print("this is not a five")
+    
---
+++ main3.py
@@ -0,0 +1,6 @@
+num=int(input("enter a number: "))
+for i in range(1,num+1):
+    if i%2!=0:
+        continue
+    else:
+        print(i)
